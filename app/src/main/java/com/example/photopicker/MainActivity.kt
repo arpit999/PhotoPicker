cffb247dd32895166e37811327564242db82cfea
arpit <arpitpatel009@gmail.com>
2024-08-16 20:39:03 -0400

    Add selected multiple images in list

--- app/src/main/java/com/example/photopicker/MainActivity.kt
+++ app/src/main/java/com/example/photopicker/MainActivity.kt
@@ -1,6 +1,5 @@
 package com.example.photopicker
 
-import android.graphics.BitmapFactory
 import android.net.Uri
 import android.os.Bundle
 import android.util.Log
@@ -9,6 +8,7 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.activity.result.PickVisualMediaRequest
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.activity.result.contract.ActivityResultContracts.PickMultipleVisualMedia
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -25,7 +25,6 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.asImageBitmap
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -38,24 +37,32 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
 
         var singleUri by mutableStateOf(Uri.EMPTY)
-        val uriList by mutableStateOf(emptyList<Uri>())
+        var uriList by mutableStateOf(emptyList<Uri>())
 
-        val pickMedia =
-            registerForActivityResult(ActivityResultContracts.PickVisualMedia()) { uri ->
+        val pickMedia = registerForActivityResult(ActivityResultContracts.PickVisualMedia()) { uri ->
                 // Callback is invoked after the user selects a media item or closes the
                 // photo picker.
                 if (uri != null) {
                     Log.d("PhotoPicker", "Selected URI: $uri")
-                    val imageBitmap =
-                        applicationContext.contentResolver.openInputStream(uri).use { data ->
-                            BitmapFactory.decodeStream(data)
-                        }.asImageBitmap()
                     singleUri = uri
                 } else {
                     Log.d("PhotoPicker", "No media selected")
                 }
             }
 
+        // Registers a photo picker activity launcher in multi-select mode.
+        // In this example, the app lets the user select up to 3 media files.
+        val pickMultipleMedia =
+            registerForActivityResult(PickMultipleVisualMedia(3)) { uris ->
+                // Callback is invoked after the user selects media items or closes the
+                // photo picker.
+                if (uris.isNotEmpty()) {
+                    Log.d("PhotoPicker", "Number of items selected: ${uris.size}")
+                    uriList = uris
+                } else {
+                    Log.d("PhotoPicker", "No media selected")
+                }
+            }
 
         setContent {
             PhotoPickerTheme {
@@ -66,7 +73,9 @@ class MainActivity : ComponentActivity() {
                             // Launch the photo picker and let the user choose only images.
                             pickMedia.launch(PickVisualMediaRequest(ActivityResultContracts.PickVisualMedia.ImageOnly))
                         },
-                        multiImageSelection = {},
+                        multiImageSelection = {
+                            pickMultipleMedia.launch(PickVisualMediaRequest(ActivityResultContracts.PickVisualMedia.ImageOnly))
+                        },
                         displayImageList = { ImageList(singleUri, uriList) },
                         modifier = Modifier.padding(innerPadding)
                     )
@@ -74,32 +83,11 @@ class MainActivity : ComponentActivity() {
             }
         }
 
-//        // Include only one of the following calls to launch(), depending on the types
-//        // of media that you want to let the user choose from.
-//
-//        // Launch the photo picker and let the user choose images and videos.
-//        pickMedia.launch(PickVisualMediaRequest(ActivityResultContracts.PickVisualMedia.ImageAndVideo))
-//
-//        // Launch the photo picker and let the user choose only images.
-//        pickMedia.launch(PickVisualMediaRequest(ActivityResultContracts.PickVisualMedia.ImageOnly))
-//
-//        // Launch the photo picker and let the user choose only videos.
-//        pickMedia.launch(PickVisualMediaRequest(ActivityResultContracts.PickVisualMedia.VideoOnly))
-//
-//        // Launch the photo picker and let the user choose only images/videos of a
-//        // specific MIME type, such as GIFs.
-//        val mimeType = "image/gif"
-//        pickMedia.launch(
-//            PickVisualMediaRequest(
-//                ActivityResultContracts.PickVisualMedia.SingleMimeType(mimeType)
-//            )
-//        )
-
     }
 }
 
 @Composable
-fun ImageList(singleUri: Uri, uriList: List<Uri>, modifier: Modifier = Modifier) {
+fun ImageList(singleUri: Uri, uriList: List<Uri>) {
     LazyColumn(Modifier.fillMaxSize()) {
         item {
             AsyncImage(
@@ -111,10 +99,10 @@ fun ImageList(singleUri: Uri, uriList: List<Uri>, modifier: Modifier = Modifier)
         }
         items(uriList) {
             AsyncImage(
-                model = singleUri,
+                model = it,
                 contentDescription = null,
                 modifier = Modifier.fillMaxWidth(),
-                contentScale = ContentScale.Fit
+                contentScale = ContentScale.FillWidth
             )
         }
     }
